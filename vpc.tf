16f14b1e44c07348e1f966da32f37c203be54b61
gokulponnusamy <ponngokul98@gmail.com>
2024-02-27 12:28:29 +0530

    eks and vpc update

--- vpc.tf
+++ vpc.tf
@@ -1,22 +1,18 @@
-resource "aws_dynamodb_table" "state_locking" {
-  hash_key = "LockID"
-  name     = "gkekstfstatefile"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  billing_mode = "PAY_PER_REQUEST"
+provider "aws" {
+  region = "us-west-2"  # Update with your desired AWS region
 }
+
+# Define the VPC
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = var.vpc_cidr_block
-  enable_dns_support   = true
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
   enable_dns_hostnames = true
 }
 
 # Define public subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = var.public_subnet_cidr_block
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-west-2a"  # Update with desired AZ
   map_public_ip_on_launch = true
 
@@ -28,7 +24,7 @@ resource "aws_subnet" "public_subnet" {
 # Define private subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.private_subnet_cidr_block
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-2b"  # Update with desired AZ
 
   tags = {
@@ -47,28 +43,6 @@ resource "aws_internet_gateway" "my_igw" {
 
 # Attach the internet gateway to the VPC
 resource "aws_vpc_attachment" "my_vpc_attachment" {
-  vpc_id             = aws_vpc.my_vpc.id
+  vpc_id       = aws_vpc.my_vpc.id
   internet_gateway_id = aws_internet_gateway.my_igw.id
-}
-
-
-# Associate public subnet with public route table
-resource "aws_route_table_association" "public_subnet_association" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-# Create private route table
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = "Private Route Table"
-  }
-}
-
-# Associate private subnet with private route table
-resource "aws_route_table_association" "private_subnet_association" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_route_table.id
 }
