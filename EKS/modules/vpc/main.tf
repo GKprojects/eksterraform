a8e1249a95fe76eb0ee101cb57ec9a25b775919e
GokulGk <94182802+GKprojects@users.noreply.github.com>
2024-03-06 17:49:55 +0530

    Update main.tf

--- EKS/modules/vpc/main.tf
+++ EKS/modules/vpc/main.tf
@@ -84,7 +84,7 @@ resource "aws_route_table" "private" {
 
   route = [
     {
-      cidr_block                 = "10.0.64.0/19"
+      cidr_block                 = "0.0.0.0/0"
       nat_gateway_id             = aws_nat_gateway.nat.id
       carrier_gateway_id         = ""
       destination_prefix_list_id = ""
@@ -109,7 +109,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block                 = "10.0.0.0/19"
+    cidr_block                 = "0.0.0.0/0"
     gateway_id                 = aws_internet_gateway.igw.id
     nat_gateway_id             = ""
     carrier_gateway_id         = ""
